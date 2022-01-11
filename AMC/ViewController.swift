02499a56df256c4b129f2f6f94c398048dd8c32a
ana <ani_dzamelashvili@yahoo.com>
2022-01-11 22:47:55 +0400

    Initial Commit

---
+++ AMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMC
+//
+//  Created by Ana Dzamelashvili on 1/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
